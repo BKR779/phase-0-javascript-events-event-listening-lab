86993abc585f47629a64bfed04cbdaa1d1d2f404
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-03-02 00:25:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const btn = document.getElementById("button");
+const div = document.querySelector("body div");
+
 function addingEventListener() {
+  btn.addEventListener("click", () => alert("Ayo!"));
+  div.addEventListener("mouseenter", function () {
+    button.disabled = true;
+  });
+  div.addEventListener("mouseleave", function () {
+    button.disabled = false;
+  });
 }
+
+addingEventListener();
